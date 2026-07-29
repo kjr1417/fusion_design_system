a828c45f760ee18e920618039b262c34e94a1048
Kyle Ray <kjr1417@gmail.com>
2026-07-29 11:46:20 -0400

    Add files via upload

--- components/feedback/Banner.jsx
+++ components/feedback/Banner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ICONS = {
-  info: "M6 2a1 1 0 1 1 0 2 1 1 0 0 1 0-2m1 3v5H5V5z",
+  info: "M0 0h12v12H0zm6 2a1 1 0 1 1 0 2 1 1 0 0 1 0-2m1 3v5H5V5z",
   success: "m3.871 8.033 6.187-6.187 1.06 1.06-7.247 7.248-2.99-2.99 1.06-1.061z",
   warning: "m6 0 6 12H0zM5 5h2v3H5zm2 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0",
   error: "M9 0H3L0 3v6l3 3h6l3-3V3zM7 2H5v5h2zm-1 8a1 1 0 1 0 0-2 1 1 0 0 0 0 2",
